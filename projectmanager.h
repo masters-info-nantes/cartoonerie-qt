2545db4940456b363a41d4aac79ed7439e9f5454
Clement TEK <E106179M@S042V2pc16.ensinfo.sciences.univ-nantes.prive>
2015-01-26 16:54:21 +0000

    Header for project manager

---
+++ projectmanager.h
@@ -0,0 +1,56 @@
+#ifndef PROJECTMANAGER_H
+#define PROJECTMANAGER_H
+#include <QVector>
+#include "project.h"
+#include "abstractoperator.h"
+#include "equaloperator.h"
+#include "loweroperator.h"
+#include "greateroperator.h"
+#include <QDir>
+#include <QDomDocument>
+#include <QCoreApplication>
+/**
+* @class ProjectManager
+* @brief manage projects
+*/
+class ProjectManager
+{
+/**
+* @brief List of projects
+*/
+QVector<Project*>* projects;
+/**
+* @brief Current project
+*/
+Project* currentProject;
+public:
+/**
+* @brief Default constructor
+*/
+ProjectManager();
+/**
+* @brief getProject
+* @return project list
+*/
+QVector<Project*>* getProjects();
+/**
+* @brief getCurrentProject
+* @return project object
+*/
+Project* getCurrentProject();
+/**
+* @brief getName
+* @param index : index of projects list
+*/
+void setCurrentProject(int index);
+/**
+* @brief updateProjects
+*/
+void updateProjects();
+/**
+* @brief addProject
+* @param dir : project directory
+*/
+void addProject(QDir* dir);
+};
+#endif // PROJECTMANAGER_H
