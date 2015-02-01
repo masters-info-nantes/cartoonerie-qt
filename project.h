46d4c63068c4b8997baa6fa50793959ef750bbef
Erilien <erilien@gmail.com>
2015-02-01 15:50:53 +0100

    Can't be tested now

--- project.h
+++ project.h
@@ -1,1 +1,48 @@
 
+#ifndef PROJECT_H
+#define PROJECT_H
+#include <QString>
+#include <QVector>
+/**
+* @class Project
+* @brief manage pawns type and rules
+*/
+class Project
+{
+/**
+* @brief Project name
+*/
+QString name;
+/**
+* @brief Project description
+*/
+QString description;
+/**
+* @brief Project version
+*/
+QString version;
+public:
+/**
+* @brief Create a new project
+* @param aName project name
+* @param aDescription project description
+* @param aVersion project version
+*/
+Project(QString aName, QString aDescription, QString aVersion);
+/**
+* @brief getName
+* @return project name
+*/
+QString getName();
+/**
+* @brief getDescription
+* @return project description
+*/
+QString getDescription();
+/**
+* @brief getVersion
+* @return project version
+*/
+QString getVersion();
+};
+#endif // PROJECT_H
