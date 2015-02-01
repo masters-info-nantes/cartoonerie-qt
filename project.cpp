2db731bd8c71a7fe72cbea6bb131d0d8f1f3941a
Erilien <erilien@gmail.com>
2015-02-01 15:50:41 +0100

    Can't be tested now

--- project.cpp
+++ project.cpp
@@ -1,1 +1,27 @@
+#include "project.h"
+
+Project::Project(QString aName, QString aDescription, QString aVersion)
+{
+    this->name = aName;
+    this->description = aDescription;
+    this->version = aVersion;
+}
+
+QString Project::getName()
+{
+    return this->name;
+}
+
+QString Project::getDescription()
+{
+    return this->description;
+}
+
+QString Project::getVersion()
+{
+    return this->version;
+}
+
+
+
 
